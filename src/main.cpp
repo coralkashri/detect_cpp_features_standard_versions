f7a7b26e594a2ec5f585029c466b3f9db8080217
Coral Kashri <coralkashri@gmail.com>
2024-01-21 00:17:30 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,3 @@
-#define LLVM_DISABLE_ABI_BREAKING_CHECKS_ENFORCING 1
-
 #include <clang/Tooling/Tooling.h>
 #include <clang/Tooling/CommonOptionsParser.h>
 #include <clang/Frontend/FrontendActions.h>
@@ -7,10 +5,21 @@
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/Frontend/CompilerInstance.h>
 
+/*#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"*/
+
 #include <iostream>
+#include <string>
+#include <vector>
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::vector<std::string> strs;
+    strs.push_back("AAA");
+    int a = 5;
     return 0;
 }
