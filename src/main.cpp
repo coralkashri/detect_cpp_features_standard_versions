9e3c585565f7158111458aee0d460146556c2cbd
Coral Kashri <coralkashri@gmail.com>
2024-01-21 00:43:05 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@
 #include <clang/ASTMatchers/ASTMatchFinder.h>
 #include <clang/Frontend/CompilerInstance.h>
 
-/*#include "clang/Frontend/FrontendActions.h"
-#include "clang/Tooling/CommonOptionsParser.h"
-#include "clang/Tooling/Tooling.h"
-// Declares llvm::cl::extrahelp.
-#include "llvm/Support/CommandLine.h"*/
-
 #include <iostream>
 #include <string>
 #include <vector>
