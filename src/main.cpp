1f4c055cf04bf0d054eb5f5570bf35a901b86d4d
Coral Kashri <coralkashri@gmail.com>
2024-02-03 00:58:42 +0200

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,31 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <span>
 
-int main() {
-	return 0;
+using namespace clang::tooling;
+using namespace clang::ast_matchers;
+
+class MyASTConsumer : public clang::ASTConsumer {
+public:
+	void HandleTranslationUnit(clang::ASTContext &Context) override {
+		// Implement your AST traversal and feature detection logic here.
+	}
+};
+
+class MyFrontendAction : public clang::ASTFrontendAction {
+public:
+	std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
+		clang::CompilerInstance &Compiler, llvm::StringRef InFile) override {
+		return std::make_unique<MyASTConsumer>();
+	}
+};
+
+int main(int argc, const char** argv) {
+	llvm::cl::OptionCategory category("Options");
+	auto OptionsParser = CommonOptionsParser::create(argc, argv, category);
+	ClangTool Tool(OptionsParser.get().getCompilations(), OptionsParser.get().getSourcePathList());
+
+	return Tool.run(newFrontendActionFactory<MyFrontendAction>().get());
+	//return EXIT_SUCCESS;
 }
